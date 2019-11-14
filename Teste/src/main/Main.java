f65e7a79c12fb688ed5908a51698576f8b4cc8ae
HorstGab <gabriel.horst.m@gmail.com>
2019-11-14 09:38:31 -0300

    Teste

--- Teste/src/main/Main.java
+++ Teste/src/main/Main.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.util.ArrayList;
+
+import servico.UCManterAtividade;
+import unioeste.geral.evento.bo.Atividade;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		
+		UCManterAtividade ma = new UCManterAtividade();
+		ArrayList<Atividade> atividades = new ArrayList<>();
+		
+		atividades = ma.consultaAtividade(1);
+		
+		for(int i = 0; i < atividades.size(); i++) {
+			System.out.println(atividades.get(i).getId());
+		}
 	}
 
 }
